55214a005ca450fb1eac2568f4913b3a10f57746
Seokju Na <seokju.me@gmail.com>
2023-05-28 14:38:52 +0900

    add test, color option

--- src/tracing.ts
+++ src/tracing.ts
@@ -1,7 +1,13 @@
 import { type Colorize, green, grey, red, yellow } from 'kleur/colors';
 import { diffLevel, type Level } from './level';
-import { findScopeLevel, parseScopes } from './scope';
+import { findScopeLevel, parseScopes, type Scope } from './scope';
 import { getProcessEnv } from './node';
+
+export function initTracing(scopeStr: string | Scope[]) {
+  const scopes = typeof scopeStr === 'string' ? parseScopes(scopeStr) : scopeStr;
+  (console as any).__TRACING__ = scopes;
+  return scopes;
+}
 
 const colors: Record<Level, Colorize> = {
   error: red,
@@ -10,14 +16,13 @@ const colors: Record<Level, Colorize> = {
   debug: grey,
 };
 
-export function initTracing(scopeStr: string) {
-  const scopes = parseScopes(scopeStr);
-  (console as any).__TRACING__ = scopes;
-  return scopes;
+interface TracerOptions {
+  /** @default true */
+  color?: boolean;
 }
 
 class Tracer {
-  constructor(public readonly namespace?: string) {}
+  constructor(public readonly namespace: string | undefined, private readonly options?: TracerOptions) {}
 
   error(...args: any) {
     this.log('error', ...args);
@@ -36,11 +41,13 @@ class Tracer {
   }
 
   log(level: Level, ...args: any) {
+    const { color = true } = this.options ?? {};
     const scopes = this.getScopes();
     const targetLevel = findScopeLevel(scopes, this.namespace ?? 'anything') ?? 'debug';
 
     if (diffLevel(targetLevel, level) >= 0) {
-      const prefix = colors[level](`[${this.namespace}:${level}]`);
+      const prefixStr = `[${this.namespace ?? ''}:${level}]`;
+      const prefix = color ? colors[level](prefixStr) : prefixStr;
       console.log(prefix, ...args);
     }
   }
@@ -54,6 +61,6 @@ class Tracer {
   }
 }
 
-export function tracer(namespace?: string) {
-  return new Tracer(namespace);
+export function tracer(namespace?: string, options?: TracerOptions) {
+  return new Tracer(namespace, options);
 }
